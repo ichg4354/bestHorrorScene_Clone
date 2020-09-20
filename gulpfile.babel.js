3b2e6d95182ae0427d940c71e29a5da00a4c64d2
user <60804817+ichg4354@users.noreply.github.com>
2020-09-21 00:53:55 +0900

    finish body__left scss

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,30 @@
-import sass from "gulp-sass";
 import gulp from "gulp";
+import scss from "gulp-sass";
+import csso from "gulp-csso";
+import del from "del";
 
 const paths = {
   styles: {
     src: "assets/scss/styles.scss",
-    dest: "src/styles.css",
+    dest: "src/styles",
+    watch: "assets/scss/**",
   },
 };
 
-export function style() {
+function style() {
   return gulp
     .src(paths.styles.src)
-    .pipe(sass())
+    .pipe(scss())
+    .pipe(csso())
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+function clean() {
+  return del([".gulp-scss-cache"]);
+}
+
+function watch() {
+  return gulp.watch(paths.styles.watch, style);
+}
+
+export const assets = gulp.series([style, clean, watch]);
